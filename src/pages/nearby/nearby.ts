06f85dda4a24fcdbfcb415a88b835d1ae53ec31b
unknown <dervis@sistemex.com>
2018-04-17 00:09:36 -0430

    mapa en navegador

--- src/pages/nearby/nearby.ts
+++ src/pages/nearby/nearby.ts
@@ -1,21 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  GoogleMapOptions,
-  //CameraPosition,
-  //MarkerOptions,
-  //Marker
-} from '@ionic-native/google-maps';
 /**
  * Generated class for the NearbyPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+
+ declare var google;
+ let infowindow: any;
+ let map: any;
 
 @IonicPage()
 @Component({
@@ -26,7 +21,14 @@ export class NearbyPage {
 
   lat: any;
   lng: any;
-  map: GoogleMap;
+
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  start = 'chicago, il';
+  end = 'chicago, il';
+  directionsService = new google.maps.DirectionsService;
+  directionsDisplay = new google.maps.DirectionsRenderer;
 
   constructor(
   	public navCtrl: NavController,
@@ -35,17 +37,69 @@ export class NearbyPage {
 
   }
 
-  ionViewDidLoad() {
-  	this.loadMap();
+  ionViewDidLoad() {  	//this.loadMap();
+  	
     console.log('ionViewDidLoad NearbyPage');
     this.geo.getCurrentPosition().then( pos => {
       this.lat = pos.coords.latitude;
       this.lng = pos.coords.longitude;
       console.log(this.lat,this.lng)
+      this.initMap(pos);
+      
     }).catch( err => console.log(err));
   }
 
-  loadMap(){
+  initMap(location) {
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 12,
+      center: {lat: this.lat, lng: this.lng}
+    });
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: {lat: location.coords.latitude, lng: location.coords.longitude},
+      radius: 1000,
+      type: ['store']
+    }, (results,status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          this.createMarker(results[i]);
+        }
+      }
+    });
+    this.directionsDisplay.setMap(this.map);
+    var myplace = {lat: this.lat, lng: this.lng};
+    this.createMarker(myplace)
+  }
+
+  createMarker(place) {
+	  var placeLoc = place.geometry.location;
+	  var marker = new google.maps.Marker({
+	    map: map,
+	    position: placeLoc
+	  });
+
+	  google.maps.event.addListener(marker, 'click', function() {
+	    infowindow.setContent(place.name);
+	    infowindow.open(map, this);
+	  });
+	}
+
+  calculateAndDisplayRoute() {
+    this.directionsService.route({
+      origin: this.start,
+      destination: this.end,
+      travelMode: 'DRIVING'
+    }, (response, status) => {
+      if (status === 'OK') {
+        this.directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
+  /*loadMap(){
   let mapOptions: GoogleMapOptions = {
     camera: {
       target: {
@@ -85,7 +139,7 @@ export class NearbyPage {
     .catch(error =>{
       console.log(error);
     });
-  }
+  }*/
 
 
 }
