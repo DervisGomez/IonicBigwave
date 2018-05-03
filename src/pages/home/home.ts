019b2c835766617196f055958172f63c533dbb5e
DervisGomez <dervis@sistemex.com>
2018-05-03 18:46:14 -0430

    Ajustes 1, 2 y 15 de la revision

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Events, AlertController } from 'ionic-angular';
 import { ListPerfilesPage } from '../list-perfiles/list-perfiles';
 import { IndependientsPage } from '../independients/independients';
+import { Storage } from '@ionic/storage';
 import { SellersPage } from '../sellers/sellers';
 import { PymesPage } from '../pymes/pymes';
+
 
 
 @Component({
@@ -11,13 +13,95 @@ import { PymesPage } from '../pymes/pymes';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  user: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public storage: Storage,
+    public events: Events,    
+    public alertCtrl: AlertController) {
+    this.checkLogin();
+    this.events.subscribe("userLogin", (user) => {
+      this.user = user;
+      console.log("events in perfil", this.user);
+    }); 
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
+  }
+
+  crearTienda(){
+    if(this.user==null){
+      let confirm = this.alertCtrl.create({
+        title: 'Crear Tienda',
+        message: 'Para crear tu tienda debes registrarte',
+        buttons: [
+          {
+            text: 'ok',
+            handler: () => {
+              this.navCtrl.parent.select(2); 
+              console.log('Disagree clicked');
+            }
+          }
+        ]
+      });
+      confirm.present();      
+    }else{
+
+    }
+  }
+
+  crearProfesional(){
+    if(this.user==null){
+      let confirm = this.alertCtrl.create({
+        title: 'Crear Profesional',
+        message: 'Para mostrar tu profesión debes registrarte',
+        buttons: [
+          {
+            text: 'ok',
+            handler: () => {
+              this.navCtrl.parent.select(2); 
+              console.log('Disagree clicked');
+            }
+          }
+        ]
+      });
+      confirm.present();      
+    }else{
+      
+    }
+  }
+
+  crearVender(){
+    if(this.user==null){
+      let confirm = this.alertCtrl.create({
+        title: 'Deseas Vender',
+        message: 'Para vender debes registrarte',
+        buttons: [
+          {
+            text: 'ok',
+            handler: () => {
+              this.navCtrl.parent.select(2); 
+              console.log('Disagree clicked');
+            }
+          }
+        ]
+      });
+      confirm.present();      
+    }else{
+      
+    }
+  }
+
+  checkLogin() {
+    this.storage.get('user').then((user) => {
+      if (user) {
+        this.user = JSON.parse(user);
+      }else{
+        //this.navCtrl.setRoot(LoginPage,{data: PerfilPage});
+      }
+    });//storage user
   }
 
   goList(perfil){
